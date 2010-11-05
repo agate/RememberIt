6fca37286b8ee0c7d7ed682e6617a99974933ed2
agate <agate.hao@gmail.com>
2010-11-05 15:15:35 +0800

    implemented mobile dict book

---
+++ public/javascripts/init.js
@@ -0,0 +1,4 @@
+FACTUAL = {
+  API_KEY:    'Jo8cwSHeGQNIWoWRWt569vHS1JiAtiCIy4pbsr9THxmaxdj008fN7QH9rR5lsnMb',
+  API_DOMAIN: 'www.factual.com'
+};
---
+++ public/javascripts/mobile.js
@@ -0,0 +1,81 @@
+$FL.$(function () {
+  new RememberIt();
+});
+
+var TABLE_KEY  = 'zILDwx';
+var RememberIt = function () { this.initialize(); }; RememberIt.prototype = {
+  initialize: function () {
+    this.table = new Factual.V2.Core.Table(TABLE_KEY);
+    this.query = {
+      limit:   1,
+      offset:  0,
+      sort:    [
+        {
+          fieldId: 3672271,
+          asc: false
+        },
+        {
+          fieldId: 3672268,
+          asc: true
+        }
+      ]
+    };
+
+    this.initHTML();
+    this.registerEvents();
+    this.read();
+  },
+
+  initHTML: function () {
+    this.$word    = $FL.$('#word');
+    this.$desc    = $FL.$('#desc');
+    this.$example = $FL.$('#example');
+
+    this.$prev    = $FL.$('[role=button]').eq(0);
+    this.$next    = $FL.$('[role=button]').eq(1);
+  },
+
+  registerEvents: function () {
+    this.$prev.click((function (self) {
+      return function (data) {
+        self.prev();
+      };
+    })(this));
+    this.$next.click((function (self) {
+      return function (data) {
+        self.next();
+      };
+    })(this));
+  },
+
+  prev: function () {
+    this.query.offset -= 1;
+
+    if (this.query.offset < 0) {
+      this.query.offset = 0;
+      return;
+    }
+
+    this.read();
+  },
+
+  next: function () {
+    this.query.offset += 1;
+    this.read();
+  },
+
+  read: function () {
+    this.table.read(this.query, (function (self) {
+      return function (data) {
+        self.loaded(data);
+      };
+    })(this));
+  },
+
+  loaded: function (data) {
+    var row = data.getRow(0);
+    this.$word.text(row.getVal('idx', 1) || 'none');
+    this.$desc.text(row.getVal('idx', 2) || 'none');
+    this.$example.text(row.getVal('idx', 3) || 'none');
+  }
+};
